3de9508e8d02352584829491fcf41673651eb859
henrique <hemagso@gmail.com>
2019-03-07 12:35:12 -0500

    Adjusted docstrings

--- lobsang/levels.py
+++ lobsang/levels.py
@@ -1,4 +1,16 @@
+""""Univariate distribution analysis
+
+This module contains methods to identify and deal with different measurement levels. It contains the following
+methods:
+
+- infer_measurement_level: Determines the measurement level of a feature from the data.
+"""
+
+# Author: Henrique Magalhães Soares
+# Licence: MIT
+
 import numpy as np
+
 from typing import Union, AnyStr
 
 MEASUREMENT_LEVELS = ["nominal", "interval"]
@@ -16,9 +28,7 @@ def infer_measurement_level(arr: np.array, method: AnyStr = "count_unique", **kw
     This classification is a simplification of Stanley Smith Stevens Level of Measurement classification.
     More about it can be read in this Wikipedia article (https://en.wikipedia.org/wiki/Level_of_measurement)
 
-    To classify correcly between these measurement levels one usually has to have knowledge about the process
-    generating the data. However, sometimes this measurement levels are reflected in the data itself. To make
-    the inference, this function currently has 1 available method:
+    The following methods are available for use:
 
     - count_unique: This method decide the measurement level based on the number of unique values present on the
         data. For more detais about it, see the documentation for the _infer_measurement_by_count method.
@@ -40,6 +50,31 @@ def infer_measurement_level(arr: np.array, method: AnyStr = "count_unique", **kw
 
 
 def _infer_measurement_by_count(arr: np.array, threshold: int = 10, use_first: Union[None, int] = None) -> str:
+    """"Infers the measurement scale of a feature by using the count method
+
+    This method uses the fact that interval measurement level variables usually are real valued features
+    and, therefore, have a lot of different values. In contrast to that, nominal variables usually are the
+    result of discrete categorization, having a smaller set of unique values.
+
+    This is of course not a rule. Counts are integer valued on the interval scale, and may have few values.
+    ZIP codes are nominal level features with lot's of unique values. It's always best to understand your
+    data generating process, assigning it the appropriate measurement level.
+
+    This function counts the number of unique values in a numpy array, and classifies it as interval if
+    this number is above a threshold.
+
+    Parameters
+    ----------
+    arr: Numpy array containing the data
+    threshold: Threshold of unique values for the classification. Default: 10
+    use_first: Number of elements on the array to be used when counting unique values. Default: None (Use all values)
+
+    Returns
+    ----------
+    measurement_level: String with one of these two values: "interval", "nominal"
+
+
+    """
     if use_first:
         arr = arr[0:use_first]
     unique_count = len(np.unique(arr))
